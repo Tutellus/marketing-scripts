f0294967dfd681dcde0a4125cf56cb2a3d1b7c89
David Nieto <david.ngd@gmail.com>
2023-06-14 17:42:19 +0200

    Update head.js

--- gtm/head.js
+++ gtm/head.js
@@ -1,16 +1,18 @@
-window.dataLayer = window.dataLayer || []
-function gtag() {
-  dataLayer.push(arguments)
-}
-gtag("js", new Date())
-gtag("config", "GTM-TRSCXQ", {
-  anonymize_ip: true,
-  cookie_expires: 0,
-  cookie_flags: "max-age=0;secure;samesite=none",
-})
-gtag("consent", "default", {
-  ad_storage: "denied",
-  analytics_storage: "denied",
-  functionality_storage: "denied",
-  security_storage: "denied",
-})
+window.addEventListener('load', function() {
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  gtag("js", new Date());
+  gtag("config", "GTM-TRSCXQ", {
+    anonymize_ip: true,
+    cookie_expires: 0,
+    cookie_flags: "max-age=0;secure;samesite=none",
+  });
+  gtag("consent", "default", {
+    ad_storage: "denied",
+    analytics_storage: "denied",
+    functionality_storage: "denied",
+    security_storage: "denied",
+  });
+});
