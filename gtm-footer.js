d778dd68e6665bf5fa70e80fc29491edb35fde0a
David Nieto <david.ngd@gmail.com>
2023-06-14 15:37:33 +0200

    Update gtm-footer.js

--- gtm-footer.js
+++ gtm-footer.js
@@ -1,6 +1,6 @@
 function insertGTM() {
   document.write(
-    '<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5WTSFXC" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>'
+    '<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TRSCXQ" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>'
   )
 }
 
