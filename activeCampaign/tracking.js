686442ebbfc708e2188762b1ee752a068676330f
David Nieto <david.ngd@gmail.com>
2023-06-16 09:18:12 +0200

    Update tracking.js

--- activeCampaign/tracking.js
+++ activeCampaign/tracking.js
@@ -22,3 +22,15 @@
 vgo("setAccount", "28224030")
 vgo("setTrackByDefault", true)
 vgo("process")
+
+// phone validation
+if (no_error && elem.name == "phone") {
+  if (
+    !value.match(/^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/i) ||
+    value.match(/^\D*(\d)(?:\D*|\1)*$/i)
+  ) {
+    elem.className = elem.className + " _has_error"
+    no_error = false
+    tooltip = create_tooltip(elem, "Introduce un número de teléfono correcto.")
+  }
+}
